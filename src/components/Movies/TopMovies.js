9adc6a8d46467f37cbbe9473e169a5840f67a4da
Jane-Kotovich <jenyakotovich@gmail.com>
2022-02-14 08:52:36 +1000

    Fixed styles for home page

--- src/components/Movies/TopMovies.js
+++ src/components/Movies/TopMovies.js
@@ -1,14 +1,25 @@
 import styles from "./TopMovies.module.css";
 import { Link } from "react-router-dom";
 const TopMovies = (props) => {
-  let topTen = props.movies.slice(0, 7);
+  let topTen = props.movies.slice(0, 10);
   return (
     <>
       <ul className={styles.topmovieList}>
         {topTen.map((m) => (
           <li key={m.id}>
-            <img src={m.image} alt="Movie poster" />
-            <Link to={`./${m.id}`}>More info</Link>
+            <Link to={`./${m.id}`}>
+              <img
+                src={m.image}
+                alt="Movie poster"
+                className={styles.topMovieImg}
+              />
+            </Link>
+            <Link to={`./${m.id}`} className={styles.link}>
+              <div>
+                <h4>{m.title}</h4>
+                <h5>{m.year}</h5>
+              </div>
+            </Link>
           </li>
         ))}
       </ul>
