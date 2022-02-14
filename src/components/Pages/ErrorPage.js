4f962a2752ae8abb2eafe3f2aca6b9d559989cb2
Jane-Kotovich <jenyakotovich@gmail.com>
2022-02-14 10:54:23 +1000

    Fixed error page css

--- src/components/Pages/ErrorPage.js
+++ src/components/Pages/ErrorPage.js
@@ -1,6 +1,8 @@
 import { useDispatch, useSelector } from "react-redux";
 import { errorActions } from "../store/error";
 import { useNavigate } from "react-router-dom";
+import errorImg from "../../assets/error.png";
+import styles from "./ErrorPage.module.css";
 
 const ErrorPage = () => {
   const isError = useSelector((state) => state.error.isError);
@@ -14,10 +16,12 @@ const ErrorPage = () => {
 
   return (
     <>
-      <div>
-        <h1>BIG errror</h1>
-        <h2>{isError}</h2>
-        <button onClick={cleanErrorHandler}>CLEAN ERROR</button>
+      <div className={styles.mainError}>
+        <img src={errorImg}></img>
+        <h1>{isError}</h1>
+        <button onClick={cleanErrorHandler}>
+          Click me for trying one more time!
+        </button>
       </div>
     </>
   );
