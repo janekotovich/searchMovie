9355fc31450e33f0f50d18a28c458f47d5077d9d
Jane-Kotovich <jenyakotovich@gmail.com>
2022-02-20 20:23:40 +1000

    Profile page
    Added ability to remove from liked movies in personal page

--- src/components/Pages/ProfilePage.js
+++ src/components/Pages/ProfilePage.js
@@ -4,16 +4,28 @@ import { favActions } from "../store/fav";
 const ProfilePage = () => {
   const dispatch = useDispatch();
   const liked = useSelector((state) => state.favs.favourites);
+  console.log(liked);
 
   return (
     <>
-      <div>
-        <h1> Movie I liked </h1>
+      <div className={styles.profilePageDiv}>
+        <h1> Movies I liked </h1>
         <ul className={styles.personalMovies}>
           {liked.map((m) => (
             <li key={m.id}>
-              <h2>{m.title}</h2>
               <img src={m.image} />
+              <div>
+                <h2>{m.title}</h2>
+                <button
+                  onClick={() => {
+                    if (liked.some((mov) => mov.id === m.id)) {
+                      dispatch(favActions.removeFromFav(m));
+                    }
+                  }}
+                >
+                  Remove me
+                </button>
+              </div>
             </li>
           ))}
         </ul>
