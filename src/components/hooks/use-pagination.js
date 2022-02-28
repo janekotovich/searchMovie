88b735c7d2f983650b2c94c477bdf5ba9d7fc125
Jane-Kotovich <jenyakotovich@gmail.com>
2022-02-28 14:53:13 +1000

    Pagination
    Case 1 - when number of the pages is less then the one we want to show

--- src/components/hooks/use-pagination.js
+++ src/components/hooks/use-pagination.js
@@ -6,12 +6,24 @@ const usePagination = ({
   siblingCount = 1,
   currentPage,
 }) => {
-  const paginationRange = useMemo(() => {}, [
-    totalCount,
-    pageSize,
-    siblingCount,
-    currentPage,
-  ]);
+  const paginationRange = useMemo(() => {
+    const totalPageCount = Math.ceil(totalCount / pageSize);
+    const totalPageNumbers = siblingCount + 5;
+    const range = (start, end) => {
+      let length = end - start + 1;
+      return Array.from({ length }, (_, i) => i + start);
+    };
+
+    if (totalPageNumbers >= totalPageCount) {
+      return range(1, totalPageCount);
+    }
+    const leftSiblingIndex = Math.max(currentPage - siblingCount, 1);
+    const rightSiblingIndex = Math.min(currentPage + siblingCount, totalCount);
+    const shouldShowLeftDots = leftSiblingIndex > 2;
+    const shouldShowRightDots = rightSiblingIndex < totalPageCount - 2;
+    const fisrtPageIndex = 1;
+    const lastPageIndex = totalPageCount;
+  }, [totalCount, pageSize, siblingCount, currentPage]);
   return <></>;
 };
 export default usePagination;
